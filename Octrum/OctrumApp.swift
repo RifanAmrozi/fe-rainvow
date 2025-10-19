9a146ce4ab924e81a0ccef0a9258e5c2c3d903a8
Marcelinus Gerardo <marcelinus.gerardo@ui.ac.id>
2025-10-19 19:28:48 +0700

    chore: initial project setup

---
+++ Octrum/OctrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  OctrumApp.swift
+//  Octrum
+//
+//  Created by Marcelinus Gerardo on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct OctrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
